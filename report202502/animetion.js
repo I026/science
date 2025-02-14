b2e09155ba2ea5b325c921b4240cca2887b71a1f
I026 <chiaki.kogretch@gmail.com>
2025-02-14 18:23:15 +0900

    Add files via upload

--- report202502/animetion.js
+++ report202502/animetion.js
@@ -774,9 +774,9 @@ function scrollEvent_Content24_1(content, scrollThreshold) {
 
 scrollEvent_Top(topContent, 1.2);
 scrollEvent_Content1_1(content1, .5);
-scrollEvent_Content1_2(content1, .9);
-scrollEvent_Content2_1(content2, .4);
-scrollEvent_Content2_2(content2, .7);
+scrollEvent_Content1_2(content1, .8);
+scrollEvent_Content2_1(content2, .3);
+scrollEvent_Content2_2(content2, .9);
 scrollEvent_Content2_3(content2, 1);
 scrollEvent_Content3_1(content3, .1);
 scrollEvent_Content3_2(content3, .2);
